6df14925203009be7dfd812ce61d051084f267df
DropoutCoder-true <vighneshzagade4@gmail.com>
2024-11-27 10:28:56 +0530

    Added fetch products funtionality and implemented filtering & sorting methods

--- server/controllers/ProductController.js
+++ server/controllers/ProductController.js
@@ -28,9 +28,42 @@ export const createProduct = async (req, res) => {
 
 export const fetchProducts = async (req, res) => {
   try {
-    const products = await Product.find();
+    const { search, category, price, page } = req.query;
+    const filter = {};
+
+    if (search) {
+      filter.title = {
+        $regex: search,
+        $options: "i",
+      };
+    }
+
+    if (price) {
+      filter.price = {
+        $gte: Number(price), // to filter products which are greater than the price
+      };
+    }
+
+    if (category) filter.category = category;
+
+    const countProducts = await Product.countDocuments(); // to give a count of all products
+    const limit = 4; // to display 4 products in one page
+    const skip = (page - 1) * limit;
+    const totalPages = Math.ceil(countProducts / limit);
+
+    const products = await Product.find(filter)
+      .sort("-createdAt")
+      .limit(limit)
+      .skip(skip);
+
+    const categories = await Product.distinct("category"); // this will give all the distinct categories
+    const mostSelling = await Product.find().sort({ sold: -1 }).limit(3); // this is just to show our top 3 best selling products
+
     res.status(200).json({
       products,
+      totalPages,
+      categories,
+      mostSelling,
     });
   } catch (error) {
     res.status(500).json({
