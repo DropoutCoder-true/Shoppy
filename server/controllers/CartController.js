3fd259c6ccde8c6a7e08eb7b8519804b9ee30b64
DropoutCoder-true <vighneshzagade4@gmail.com>
2024-11-27 23:00:28 +0530

    added fetch card controller

--- server/controllers/CartController.js
+++ server/controllers/CartController.js
@@ -41,3 +41,25 @@ export const addToCart = async (req, res) => {
     res.status(500).json({ message: error.message });
   }
 };
+
+export const fetchCart = async (req, res) => {
+  try {
+    const cart = await Cart.find({ user: req.user._id }).populate("product");
+    const sumOfQuantities = cart.reduce(
+      (total, item) => total + item.quantity,
+      0
+    );
+    let subTotal = 0;
+
+    cart.forEach((i) => {
+      const itemSubTotal = i.product.price * i.quantity;
+      subTotal += itemSubTotal;
+    });
+
+    res.status(200).json({ cart, sumOfQuantities, subTotal });
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
--- server/routes/cart.js
+++ server/routes/cart.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { addToCart } from "../controllers/CartController.js";
+import { addToCart, fetchCart } from "../controllers/CartController.js";
 import { isAuth } from "../middlewares/isAuth.js";
 
 const router = express.Router();
 
 router.post("/cart/new", isAuth, addToCart);
+router.get("/cart/all", isAuth, fetchCart);
 
 export default router;
