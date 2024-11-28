c9ff7d7cbbe7aef773eedbdae9493280bd171250
DropoutCoder-true <vighneshzagade4@gmail.com>
2024-11-28 06:54:25 +0000

    implemented update cart api functionality

--- server/controllers/CartController.js
+++ server/controllers/CartController.js
@@ -78,3 +78,46 @@ export const removeFromCart = async (req, res) => {
     });
   }
 };
+
+export const updateCart = async (req, res) => {
+  try {
+    const { action } = req.query;
+
+    if (action === "inc") {
+      const { id } = req.body;
+      const cart = await Cart.findById(id).populate("product");
+
+      if (cart.quantity < cart.product.stock) {
+        cart.quantity++;
+        await cart.save();
+      } else {
+        res.status(400).json({
+          message: "Out of Stock",
+        });
+      }
+      res.status(200).json({ message: "Cart Updated" });
+    }
+
+    if (action === "dec") {
+      const { id } = req.body;
+      const cart = await Cart.findById(id);
+
+      if (cart.quantity > 1) {
+        cart.quantity--;
+        await cart.save();
+      } else {
+        return res.status(400).json({
+          message: "You have only one item",
+        });
+      }
+
+      res.status(200).json({
+        message: "Cart Updated",
+      });
+    }
+  } catch (error) {
+    res.status(500).json({
+      message: error.message,
+    });
+  }
+};
--- server/routes/cart.js
+++ server/routes/cart.js
@@ -3,6 +3,7 @@ import {
   addToCart,
   fetchCart,
   removeFromCart,
+  updateCart,
 } from "../controllers/CartController.js";
 import { isAuth } from "../middlewares/isAuth.js";
 
@@ -11,5 +12,6 @@ const router = express.Router();
 router.post("/cart/new", isAuth, addToCart);
 router.get("/cart/all", isAuth, fetchCart);
 router.delete("/cart/:id", isAuth, removeFromCart);
+router.put("/cart", isAuth, updateCart);
 
 export default router;
