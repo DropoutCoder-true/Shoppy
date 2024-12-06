073e3660f72b36c7f0fdfeecca7e412ab16cf4ad
DropoutCoder-true <vighneshzagade4@gmail.com>
2024-12-06 16:01:04 +0000

    added 3 new images for carousel

--- app/src/components/Slider.jsx
+++ app/src/components/Slider.jsx
@@ -5,21 +5,21 @@ const Slider = () => {
     <Carousel>
       <Carousel.Item>
         <img
-          src="/shopping-bag.jpg"
+          src="/image-1.jpg"
           alt="shopping-bag-image"
           className="d-block w-100"
         />
       </Carousel.Item>
       <Carousel.Item>
         <img
-          src="/shopping-bag.jpg"
+          src="/image-2.jpg"
           alt="shopping-bag-image"
           className="d-block w-100"
         />
       </Carousel.Item>
       <Carousel.Item>
         <img
-          src="/shopping-bag.jpg"
+          src="/image-3.jpg"
           alt="shopping-bag-image"
           className="d-block w-100"
         />
