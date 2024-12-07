9b50926ebcc2b12cd28cb782b67efb68cfef54db
DropoutCoder-true <vighneshzagade4@gmail.com>
2024-12-07 13:17:17 +0000

    added all products to the products page

--- app/src/pages/Products.jsx
+++ app/src/pages/Products.jsx
@@ -1,5 +1,7 @@
-import { Badge, Container } from "react-bootstrap"
+import { Badge, Container, Row } from "react-bootstrap"
 import { ProductData } from "../context/ProductContext"
+import ProductCard from "../components/ProductCard"
+import Loader from "../components/Loader"
 
 const Products = () => {
   const {products, loading} = ProductData()  
@@ -9,6 +11,11 @@ const Products = () => {
       <h4> 
         Our Products <Badge bg="secondary"> All Products </Badge>
       </h4>
+      {
+        loading ? <Loader/> : <Row className="justify-content-center" style={{gap: "1rem"}}>
+          { products && products.length > 0 ? (products.map((e) =>  <ProductCard key={e._id} product={e}/>)) : (<p>No Products Yet</p>)}
+        </Row>
+      }
     </Container>
   )
 }
