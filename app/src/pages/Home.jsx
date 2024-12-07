94bfff807b81353f1e05671e3882b6a230226a8a
DropoutCoder-true <vighneshzagade4@gmail.com>
2024-12-07 11:26:26 +0000

    added a loader component from bootstrrap

--- app/src/pages/Home.jsx
+++ app/src/pages/Home.jsx
@@ -2,6 +2,7 @@ import { Badge, Container, Row } from "react-bootstrap";
 import Slider from "../components/Slider";
 import { ProductData } from "../context/ProductContext";
 import ProductCard from "../components/ProductCard";
+import Loader from "../components/Loader";
 
 const Home = () => {
   const {topProducts, loading} = ProductData(); 
@@ -13,11 +14,13 @@ const Home = () => {
           Our Products <Badge bg="secondary">Top Selling</Badge>
         </h4>
 
-        <Row className="justify-content-center" style={{gap: "1rem"}}>
+        {
+          loading ? <Loader/> : <Row className="justify-content-center" style={{gap: "1rem"}}>
           {
             topProducts && topProducts.length > 0 ? (topProducts.map((e) => <ProductCard key={e._id} product={e}/>)) : (<p>No Products Yet.</p>)
           }          
         </Row>
+       }
       </Container>
     </div>
   );
