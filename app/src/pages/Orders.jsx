6dc8c5a617aabdd1f20447fb5bd3d5b012d40f81
DropoutCoder-true <vighneshzagade4@gmail.com>
2024-12-12 12:25:50 +0530

    fixed the get all orders in OrderPage issue

--- app/src/pages/Orders.jsx
+++ app/src/pages/Orders.jsx
@@ -15,7 +15,7 @@ const Orders = () => {
       });
       setOrders(data.orders);
     } catch (error) {
-      console.log(error);
+      console.log(error.response.data.message);
     }
   }
 
--- server/routes/order.js
+++ server/routes/order.js
@@ -10,8 +10,8 @@ import { isAuth } from "../middlewares/isAuth.js";
 const router = express.Router();
 
 router.post("/order/new/cod", isAuth, newOrderCod);
+router.get("/order/all", isAuth, getAllOrder);
 router.get("/order/:id", isAuth, getMyOrder);
 router.put("/order/:id", isAuth, updateStatus);
-router.get("/order/all", isAuth, getAllOrder);
 
 export default router;
